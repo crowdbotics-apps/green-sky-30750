20ca9ed6fb33e22e81a25db48f138f0cf5831ee3
Crowdbotics <team@crowdbotics.com>
2021-09-22 17:34:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "green_sky_30750Identifier",
+  url: "https://green_sky_30750Identifier.botics.co",
+  api: "https://green_sky_30750Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
